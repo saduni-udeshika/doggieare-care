ab0109d4e878293a03d19dc674eaa2076d1aa486
saduni-udeshika <saduniudeshika0@gmail.com>
2022-09-07 22:01:48 +0530

    Add medicine page

---
+++ frontend/src/pages/medicinePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const medicinePage = () => {
+  return (
+    <div className='form'>
+    <form className="container">
+    <div>Add Medicine</div>
+    
+    <label>Medicine</label>
+    <label>QTY</label>
+    <label>Expiry Date</label>
+    <label>Category</label>
+    <label>Description</label>
+    </form>
+    </div>
+  )
+}
