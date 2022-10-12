59d681b37c0bd2199715bff5d69384d5d0c4f935
saduni-udeshika <saduniudeshika0@gmail.com>
2022-10-12 19:27:21 +0530

    Image upload to local disk

--- backend/src/routes/medicineRoute.js
+++ backend/src/routes/medicineRoute.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const {createMedicine, deleteMedicine, getMedicine, updateMedicine} = require('../controllers/medicineController.js');
-
 const router = express.Router();
+var multer  = require('multer');
 
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+var upload = multer({ storage: storage })
 router.get('/',getMedicine);
 
 router.post('/', createMedicine);
@@ -10,5 +19,15 @@ router.post('/', createMedicine);
 router.delete('/:id',  deleteMedicine);
 
 router.put('/:id', updateMedicine);
+
+router.post('/upload-medicine', upload.single('medicineImg'), async (req, res, next) =>{
+    try {
+        // const { medicineImg: { path, mimetype } } = req;
+        // const result = await ;
+        // res.status(result.status || 200).send(result.data);
+      } catch (err) {
+        next(err);
+      }
+  })
 
 module.exports = router;
