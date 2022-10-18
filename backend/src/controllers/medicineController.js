0c72a2789b333ec5c1923e78d27e53736ca1e852
saduni-udeshika <saduniudeshika0@gmail.com>
2022-10-18 20:31:47 +0530

    Search medicine api

--- backend/src/controllers/medicineController.js
+++ backend/src/controllers/medicineController.js
@@ -42,6 +42,25 @@ module.exports.createMedicine = async (req, res, _next) => {
       })
   }
 
+  module.exports.serchMedicine = async(req, res) => {
+    let result = await MedicineModel.find({
+      "$or":[
+        {
+          medicineName: {$regex: req.params.key}
+        },
+        {
+          category: {$regex: req.params.key}
+        }
+      ]
+    })
+    .then((medicine) => {
+        res.status(200).send({status: "Medicine filterd", medicine});
+    }).catch((err) => {
+        console.log(err.message);
+        res.status(500).send({status: "Error with get medicine",error: err.message});
+    })
+}
+
   module.exports.updateMedicine = async (req, res, _next) => {
       let id = req.params.id;
       const {medicineName, quantity, expDate, category, description, imgUrl} =req.body;
