49bbe262750880a6f5147f0d1f6713ed3894fcf3
saduni-udeshika <saduniudeshika0@gmail.com>
2022-10-20 22:06:56 +0530

    Create sales report

--- frontend/src/reports/salesReport.js
+++ frontend/src/reports/salesReport.js
@@ -1,8 +1,21 @@
-import React,{ useRef} from 'react';
+import React,{ useState, useEffect, useRef} from 'react';
 import { Button } from 'react-bootstrap';
 import {useReactToPrint} from 'react-to-print';
+import { getSales } from '../services/salesService';
 
 export const SalesReport = () => {
+
+  const [sales, setSales] = useState([]);
+
+  const getMedicineSales = async () => {
+    const medicineResponse = await getSales();
+    setSales(medicineResponse.data);
+  };
+
+  useEffect(() => {
+    getMedicineSales();
+  }, []);
+
     const componentRef = useRef()
     const handPrint = useReactToPrint({
         content: () => componentRef.current,
@@ -11,10 +24,12 @@ export const SalesReport = () => {
     });
 
   return (
+    <diV>
         <div ref={componentRef} style={{width: '100%', height: window.innerHeight}}>
             <h1 className='text-center my-3 border py-2'>Sales Report</h1>
             <table className='w-75 mx-auto' bordered >
                 <thead>
+                    <th></th>
                     <th>Medicine Name</th>
                     <th>Sold Quantity</th>
                     <th>Category</th>
@@ -22,10 +37,22 @@ export const SalesReport = () => {
                     <th>Remaining Amount</th>
                 </thead>
                 <tbody>
-                    
+                    {sales.map((sales, key) => {
+                        return(
+                            <tr key={key}>
+                                <td>{key + 1}</td>
+                                <td>{sales.medicineName}</td>
+                                <td>{sales.soldQuantity}</td>
+                                <td>{sales.category}</td>
+                                <td>{sales.soldPrice}</td>
+                                <td>{sales.remainingAmount}</td>
+                            </tr>
+                        )
+                    })}
                 </tbody>
             </table>
-            <Button onClick={handPrint}>Print this out</Button>
         </div>
+        <Button variant= "danger" onClick={handPrint}>Print this out</Button>
+    </diV>
   )
 }
